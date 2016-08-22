f4da3ed24ab737aacd7989450edeaa60c65aae61
patrickcrocker <patrick.crocker@gmail.com>
2016-08-21 20:59:55 -0500

    updated to use maven-resource

--- ci/build.sh
+++ ci/build.sh
@@ -2,8 +2,10 @@
 
 set -e
 
-version=`cat version/number`
+version=$(cat version/number)
 
 pushd aviato-profile
   ./mvnw clean package -DversionNumber=$version
 popd
+
+cp aviato-profile/build/libs/aviato-profile-$VERSION.jar build-output/.
