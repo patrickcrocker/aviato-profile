70be5389c336d80ab37c450f1471a62f3d16456a
patrickcrocker <patrick.crocker@gmail.com>
2016-09-05 14:38:00 -0500

    added repository param info for settings.xml

--- ci/build.sh
+++ ci/build.sh
@@ -4,8 +4,13 @@ set -e
 
 version=$(cat version/number)
 
+$args="-DversionNumber=$version"
+[ -n "$MAVEN_REPO_MIRROR" ] && args="$args -Drepository.url=$MAVEN_REPO_MIRROR";
+[ -n "$MAVEN_REPO_USERNAME" ] && args="$args -Drepository.username=$MAVEN_REPO_USERNAME";
+[ -n "$MAVEN_REPO_PASSWORD" ] && args="$args -Drepository.password=$MAVEN_REPO_PASSWORD";
+
 pushd aviato-profile
-  ./mvnw clean package -DversionNumber=$version
+  ./mvnw clean package $args
 popd
 
 cp aviato-profile/target/aviato-profile-$version.jar build-output/.
