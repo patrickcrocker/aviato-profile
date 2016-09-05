197ef3da3611ccb6aee4d066eb903e979ca05b2b
patrickcrocker <patrick.crocker@gmail.com>
2016-09-05 14:47:30 -0500

    removed $

--- ci/build.sh
+++ ci/build.sh
@@ -4,7 +4,7 @@ set -e
 
 version=$(cat version/number)
 
-$args="-DversionNumber=$version"
+args="-DversionNumber=$version"
 [ -n "$MAVEN_REPO_MIRROR" ] && args="$args -Drepository.url=$MAVEN_REPO_MIRROR";
 [ -n "$MAVEN_REPO_USERNAME" ] && args="$args -Drepository.username=$MAVEN_REPO_USERNAME";
 [ -n "$MAVEN_REPO_PASSWORD" ] && args="$args -Drepository.password=$MAVEN_REPO_PASSWORD";
