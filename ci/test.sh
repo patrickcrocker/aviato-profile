71e54a7f7f2891ff3d07a00dbeeaa02da1e18814
patrickcrocker <patrick.crocker@gmail.com>
2017-03-17 08:04:37 -0500

    added some maven stuff

--- ci/test.sh
+++ ci/test.sh
@@ -2,5 +2,14 @@
 
 set -e
 
-# TODO: do something here...
-ls -al maven-milestone
+version=$(cat version/number)
+
+args="-DversionNumber=$version"
+[ -n "$MAVEN_REPO_MIRROR" ] && args="$args -Drepository.url=$MAVEN_REPO_MIRROR";
+[ -n "$MAVEN_REPO_USERNAME" ] && args="$args -Drepository.username=$MAVEN_REPO_USERNAME";
+[ -n "$MAVEN_REPO_PASSWORD" ] && args="$args -Drepository.password=$MAVEN_REPO_PASSWORD";
+
+cd aviato-profile
+# todo: run some integration tests
+#./mvnw verify
+cd ..
