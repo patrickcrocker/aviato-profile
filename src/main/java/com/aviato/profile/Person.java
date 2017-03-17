82243b98bea9fdf813bdf3839a87d0998c677bcc
patrickcrocker <patrick.crocker@gmail.com>
2017-03-17 05:16:35 -0500

    using lombok

--- src/main/java/com/aviato/profile/Person.java
+++ src/main/java/com/aviato/profile/Person.java
@@ -1,35 +1,18 @@
 package com.aviato.profile;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import lombok.*;
 
 @Entity
+@Data
 public class Person {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Setter(AccessLevel.PROTECTED)
 	private long id;
 
 	private String firstName;
 
 	private String lastName;
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
 }
